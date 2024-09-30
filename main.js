1d228cb07da16ce65b07e398c941baf478c5c33f
Efestus31 <biagiohd@gmail.com>
2024-09-30 18:12:24 +0200

    pseudo codice e strumenti da utilizzare

---
+++ main.js
@@ -0,0 +1,20 @@
+/* Scrivi un programma che stampi i numeri da 1 a 100,
+ ma per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz
+. Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
+*/
+
+/* strumenti da usare
+* for (come ciclo di incremento)
+* if, else if, else per verificare le condizioni da soddisfare
+* console.log per stampare i vari risultati
+*/
+
+// Ciclo che va da 1 a 100
+
+// Verifico se i è un multiplo di 3 e 5, stampa fizzbuzz
+
+// Altrimenti, verifica se i è un multiplo di 3, stampa fizz
+
+// Altrimenti, verifica se i è un multiplo di 5, stampa buzz
+
+// Se non è nessuno dei precedenti stampa i
